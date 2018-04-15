f7514881f42f97231576c5a4d060a17fe02ac2e0
Tobias Rutsch <tobiasrutsch@gmail.com>
2018-04-15 22:27:23 +0200

    Kickoff_Aufgabe hizugefügt

---
+++ Kickoff_Aufgabe/Aufgabe2.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+var chalk = require('chalk');
+ 
+ fs.readFile(__dirname+"/staedte.json", function(err,data){
+ 	var staedte = JSON.parse(data);
+ 	
+ 	for(var i = 0;i < staedte.cities.length;i++){
+
+		console.log("name: "+chalk.blue(staedte.cities[i].name));
+		console.log("country: "+chalk.green(staedte.cities[i].country));
+		console.log("population: "+chalk.yellow(staedte.cities[i].population));
+ 		console.log("\n--------------------\n");	
+ 	}
+
+});
